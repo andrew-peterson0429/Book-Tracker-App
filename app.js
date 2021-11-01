168bacbe201b16b876070f4e4864e53797de4852
Andrew Peterson Account <andrew.peterson1021@gmail.com>
2021-10-31 22:44:03 -0600

    netlify still refusing to work possibly fix

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ function getBooks() {
 
     //  if want all the results response.docs.length
     .then(response => {
-        const addBookBtn = document.createElement("button");
+        /* const addBookBtn = document.createElement("button");
         addBookBtn.innerHTML = 'Add to Read List';
-        document.body.appendChild(btn);
+        document.body.appendChild(btn); */
         for (let i = 0; i < 10; i++) {
             document.getElementById('outputDiv').innerHTML += "<h2>" + response.docs[i].title + "</h2>" 
             + response.docs[i].author_name[0] + "<br><img src='https://covers.openlibrary.org/b/isbn/" 
-            + response.docs[i].isbn[0] + "-M.jpg'></img></br>" + "<btn>Add to Read List</btn>";
+            + response.docs[i].isbn[0] + "-M.jpg'></img></br>";
             
         }
 
